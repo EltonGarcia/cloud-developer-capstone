ff522911c2c777b82631ffa55f2b26f34f4a0b63
Elton Santana <elton.garcia.s@gmail.com>
2021-01-13 22:53:01 -0300

    Search note with elastic search

--- backend/src/dataLayer/notesAccess.ts
+++ backend/src/dataLayer/notesAccess.ts
@@ -2,104 +2,115 @@ import * as AWS from 'aws-sdk'
 // import * as AWSXRay from 'aws-xray-sdk'
 const AWSXRay = require('aws-xray-sdk');
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-
+import * as elasticsearch from 'elasticsearch'
+import * as httpAwsEs from 'http-aws-es'
 import { Note } from '../models/Note'
 import { UpdateNoteRequest } from '../requests/UpdateNoteRequest'
 
 const XAWS = AWSXRay.captureAWS(AWS)
+const esIndex = process.env.ES_INDEX
+const esType = process.env.ES_NOTES_TYPE
 
 export class NotesAccess {
     constructor(
         private readonly docClient: DocumentClient = createDynamoDBClient(),
         private readonly notesTable = process.env.NOTES_TABLE,
         private readonly indexName = process.env.NOTES_INDEX) {
-        }
-        
-        async createNote(item: Note): Promise<Note> {
-            await this.docClient.put({
-                TableName: this.notesTable,
-                Item: item
-            }).promise()
-            
-            return item
-        }
-        
-        async getUserNotes(userId: string): Promise<Note[]> {
-            console.log('Getting all user notes')
-            
-            const result = await this.docClient.query({
-                TableName: this.notesTable,
-                IndexName: this.indexName,
-                KeyConditionExpression: 'userId = :userId',
-                ExpressionAttributeValues: {
-                    ':userId': userId
-                }
-            }).promise()
-            
-            const items = result.Items
-            return items as Note[]
-        }
-        
-        async updateNote(noteId: string, userId: string, item: UpdateNoteRequest): Promise<void> {
-            await this.docClient.update({
-                TableName: this.notesTable,
-                Key: {
-                    noteId: noteId,
-                    userId: userId
-                },
-                UpdateExpression: 'set title = :title, content = :content, favorite = :favorite',
-                ExpressionAttributeValues: {
-                    ':title': item.title,
-                    ':content': item.content,
-                    ':favorite': item.favorite
-                }
-            }).promise()
-        }
-        
-        async deleteNote(noteId: string, userId: string): Promise<void> {
-            await this.docClient.delete({
-                TableName: this.notesTable,
-                Key: {
-                    noteId: noteId,
-                    userId: userId,
-                }
-            }).promise()
-        }
-        
-        async setAttachmentUrl(noteId: string, userId: string, attachmentUrl: string): Promise<void> {
-            await this.docClient.update({
-                TableName: this.notesTable,
-                Key: {
-                    noteId,
-                    userId,
-                },
-                UpdateExpression: 'set attachmentUrl = :attachmentUrl',
-                ExpressionAttributeValues: {
-                    ':attachmentUrl': attachmentUrl,
-                }
-            }).promise();
-        }
-        
-        async searchUserNote(userId: string, query: string): Promise<Note[]> {
-            const param = {
-                "multi_match": {
-                    "query": query + " " + userId,
-                    "type": "cross_fields",
-                    "fields": ["title", "content","userid"],
-                    "operator":   "and"
-                }
+    }
+
+    async createNote(item: Note): Promise<Note> {
+        await this.docClient.put({
+            TableName: this.notesTable,
+            Item: item
+        }).promise()
+
+        return item
+    }
+
+    async getUserNotes(userId: string): Promise<Note[]> {
+        console.log('Getting all user notes')
+
+        const result = await this.docClient.query({
+            TableName: this.notesTable,
+            IndexName: this.indexName,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {
+                ':userId': userId
             }
-            return await this.es.search({
-                index: ElasticSearchLogCategory.NEW_TODO,
-                type: 'notes',
-                body:{
-                    "query": param
-                }
-            })
+        }).promise()
+
+        const items = result.Items
+        return items as Note[]
+    }
+
+    async updateNote(noteId: string, userId: string, item: UpdateNoteRequest): Promise<void> {
+        await this.docClient.update({
+            TableName: this.notesTable,
+            Key: {
+                noteId: noteId,
+                userId: userId
+            },
+            UpdateExpression: 'set title = :title, content = :content, favorite = :favorite',
+            ExpressionAttributeValues: {
+                ':title': item.title,
+                ':content': item.content,
+                ':favorite': item.favorite
+            }
+        }).promise()
+    }
+
+    async deleteNote(noteId: string, userId: string): Promise<void> {
+        await this.docClient.delete({
+            TableName: this.notesTable,
+            Key: {
+                noteId: noteId,
+                userId: userId,
+            }
+        }).promise()
+    }
+
+    async setAttachmentUrl(noteId: string, userId: string, attachmentUrl: string): Promise<void> {
+        await this.docClient.update({
+            TableName: this.notesTable,
+            Key: {
+                noteId,
+                userId,
+            },
+            UpdateExpression: 'set attachmentUrl = :attachmentUrl',
+            ExpressionAttributeValues: {
+                ':attachmentUrl': attachmentUrl,
+            }
+        }).promise();
+    }
+
+    async searchUserNote(userId: string, query: string): Promise<any> {
+        const param = {
+            "multi_match": {
+                "query": query + " " + userId,
+                "type": "cross_fields",
+                "fields": ["title", "content", "userid"],
+                "operator": "and"
+            }
         }
+        const client = createElasticSearchClient()
+        const items = await client.search({
+            index: esIndex,
+            type: esType,
+            body: {
+                "query": param
+            }
+        })
+        return items.hits
     }
-    
-    function createDynamoDBClient() {
-        return new XAWS.DynamoDB.DocumentClient()
-    }
-    
+}
+
+function createDynamoDBClient() {
+    return new XAWS.DynamoDB.DocumentClient()
+}
+
+function createElasticSearchClient(): elasticsearch.Client {
+    const esHost = process.env.ES_ENDPOINT
+    return new elasticsearch.Client({
+        hosts: [esHost], connectionClass: httpAwsEs
+    })
+}
--- backend/src/lambda/dynamodb/elasticSearchSync.ts
+++ backend/src/lambda/dynamodb/elasticSearchSync.ts
@@ -1,16 +1,45 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda'
 import 'source-map-support/register'
 import { createLogger } from '../../utils/logger'
+import * as elasticsearch from 'elasticsearch'
+import * as httpAwsEs from 'http-aws-es'
 
 const logger = createLogger('es')
+const esHost = process.env.ES_ENDPOINT
+const esIndex = process.env.ES_INDEX
+const esType = process.env.ES_NOTES_TYPE
+
+const es = new elasticsearch.Client({
+  hosts: [ esHost ],
+  connectionClass: httpAwsEs
+})
 
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
     logger.info('Processing events batch from DynamoDB', JSON.stringify(event))
     for (const record of event.Records) {
         logger.info('Processing record', JSON.stringify(record))
-        // if (record.eventName == 'INSERT') {
-        //   const newItem = record.dynamodb.NewImage
-        // }else if(record.eventName == 'MODIFY'){
-        // }
+        logger.info(JSON.stringify(record))
+        if (record.eventName == 'INSERT') {
+            const newItem = record.dynamodb.NewImage
+            const noteId = newItem.noteId.S
+            // const userId = newItem.userId.S
+            const body = {
+                createdAt: newItem.createdAt.S,
+                noteId: newItem.noteId.S,
+                title: newItem.title.S,
+                favorite: newItem.favorite.S,
+                userId: newItem.userId.S,
+                content: newItem.content.S
+            }
+          
+            await es.index({
+                index: esIndex,
+                type: esType,
+                id: noteId,
+                body
+            })
+
+        } else if (record.eventName == 'MODIFY') {
+        }
     }
 }
